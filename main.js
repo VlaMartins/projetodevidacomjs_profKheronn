35ced218db6b662a1a0d35642a39ff9f6f3c259e
VladismaraMartins <100156223+VlaMartins@users.noreply.github.com>
2024-04-16 18:45:33 -0300

    55

--- main.js
+++ main.js
@@ -2,9 +2,9 @@ const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
 
 //iterando (percorrer elementos de uma lista) sobre a lista de botoes 
-for (let i=0; i < botoes.length; i++) {
+for (let i = 0; i < botoes.length; i++) {
     botoes[i].onclick = function () {
-        for (let j=0; j < botoes.length; j++) {
+        for (let j = 0; j < botoes.length; j++) {
             botoes[j].classList.remove("ativo");
             textos[j].classList.remove("ativo");
         }
@@ -13,19 +13,24 @@ for (let i=0; i < botoes.length; i++) {
     }
 }
 //document.querySelector("#minhaDiv p").textContent = "Olá Chuva!";
-const contadores = document.querySelectorAll (".contador");
+const contadores = document.querySelectorAll(".contador");
 const tempoAtual = new Date(); //retorna data atual do computador
 const tempoObjetivo1 = new Date("2024-04-19T00:00:00"); // formato de data padrao com hifem e um T para tempo
-let tempofinal = tempoObjetivo1 - tempoAtual; 
 
-let segundos = Math.floor (tempofinal / 1000);
-let minutos = Math.floor (segundos / 60); 
-let horas = Math.floor (minutos / 60);
-let dias = Math.floor (horas / 24); 
+function calculaTempo(tempoObjetivo) {
+    let tempofinal = tempoObjetivo - tempoAtual;
 
-segundos %=  60; // sinal de porcentagem traz o resto da divisao
-minutos %= 60;
-horas %= 24;
+    let segundos = Math.floor(tempofinal / 1000);
+    let minutos = Math.floor(segundos / 60);
+    let horas = Math.floor(minutos / 60);
+    let dias = Math.floor(horas / 24);
 
-contadores[0].textContent = dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+    segundos %= 60; // sinal de porcentagem traz o resto da divisao
+    minutos %= 60;
+    horas %= 24;
+    return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+
+}
+
+contadores[0].textContent = calculaTempo (tempoObjetivo1);
 
