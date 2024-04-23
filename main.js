1a8db0e22d28dde88a18535944fc18ce9596064b
VladismaraMartins <100156223+VlaMartins@users.noreply.github.com>
2024-04-23 19:02:08 -0300

    11

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ for (let i = 0; i < botoes.length; i++) {
 }
 //document.querySelector("#minhaDiv p").textContent = "Olá Chuva!";
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-04-26T00:00:00"); // formato de data padrao com hifem e um T para tempo
+const tempoObjetivo1 = new Date("2024-04-19T00:00:00"); // formato de data padrao com hifem e um T para tempo
 const tempoObjetivo2 = new Date("2024-07-07T00:00:00");
 const tempoObjetivo3 = new Date("2024-04-27T00:00:00");
 const tempoObjetivo4 = new Date("2024-04-23T00:00:00");
@@ -34,7 +34,13 @@ function calculaTempo(tempoObjetivo) {
     segundos %= 60; // sinal de porcentagem traz o resto da divisao
     minutos %= 60;
     horas %= 24;
-    return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+    if (tempofinal > 0){
+        return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";}
+        else{
+            return "Prazo Finalizado!"
+        }
+
+    }
 }
 
 function atualizaCronometro() {
